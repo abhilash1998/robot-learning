6f5e17563efc60ea1e3aeb85bce170333ebb0460
Abhilash1998 <abhilash19@gmail.com>
2022-04-14 16:56:35 -0400

    pose and velocity added along with laser sensors

--- turtlebot_learning/src/turtlebot3_data_collector.py
+++ turtlebot_learning/src/turtlebot3_data_collector.py
@@ -7,10 +7,11 @@ from geometry_msgs.msg import PoseStamped,Pose
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from actionlib_msgs.msg import GoalStatus
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image,CompressedImage,Imu
 import numpy as np
 from cv_bridge import CvBridge,CvBridgeError
 import cv2
+from sensor_msgs.msg import LaserScan
 # from costmap_2d import costmap_2d
 # costmap_2d.
 
@@ -19,6 +20,9 @@ class Data:
         self.bridge=CvBridge()
         self.pos_subscriber=rospy.Subscriber("/odom", Odometry, self.pose_callback)
         self.camera_subscriebr=rospy.Subscriber("/camera/image",Image,self.img_callback)
+        self.depth_subscriber=rospy.Subscriber("/camera/image/compressedDepth",CompressedImage,self.compressed_depth_img)
+        self.laser_2d_subscriber=rospy.Subscriber("/scan",LaserScan,self.lidar_2d_callback)
+        
         # self.goal_publish=rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=10)
         # self.goal=PoseStamped()
         # self.goal.header.stamp=rospy.Time.now()    
@@ -35,9 +39,18 @@ class Data:
         rospy.loginfo("Action client started")
         #self.client.wait_for_server(wait)
         self.movebase_action_client()
-        
-        
-        
+    def imu_callback(msg):
+        imu_data= msg  
+    def compressed_depth_img(self,msg):
+        np_arr = np.fromstring(msg.data, np.uint8)
+        image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        # print(depth)
+        cv2.imshow('depth',image_np)
+        cv2.waitKey(1)  
+    def lidar_2d_callback(self,msg):
+        # print(msg)
+        self.laser_data=msg.ranges
+        print(np.array(self.laser_data).shape)
     def img_callback(self,msg):
         self.bridge=CvBridge()
         try :
@@ -46,11 +59,33 @@ class Data:
             print(e)
         cv2.imshow("image",self.image_data)
         cv2.waitKey(1)
-        # rospy.signal_shutdown(1==1)
-
+       
     def pose_callback(self,msg):
         # print('msg',msg)
         rospy.logwarn("Subscribed sucessfullly")
+        position=np.zeros(3)
+        position[0]=msg.pose.pose.position.x
+        position[1]=msg.pose.pose.position.y
+        
+        quaternion=np.zeros(4)
+        quaternion[0]=msg.pose.pose.orientation.x
+        quaternion[1]=msg.pose.pose.orientation.y
+        quaternion[2]=msg.pose.pose.orientation.z
+        quaternion[3]=msg.pose.pose.orientation.w
+        
+        linear_velocity=np.zeros(3)
+        linear_velocity[0]=msg.twist.twist.linear.x
+        linear_velocity[1]=msg.twist.twist.linear.y
+        # linear_velocity[2]=msg.twist.twist.linear.z
+        angular_speed=np.zeros(3)
+        angular_speed[0]=msg.twist.twist.angular.x
+        angular_speed[1]=msg.twist.twist.angular.y
+        angular_speed[2]=msg.twist.twist.angular.z
+        
+        print(angular_speed)
+        
+        # quaternion[3]=msg.twist.twist.linear.w
+        
     
         # print(self.goal)
         rospy.sleep(2)
@@ -62,7 +97,7 @@ class Data:
         self.client.send_goal(self.goal,self.completed_action_callback,self.active_action_callback,self.feedback_action_server_callback)
     def goal_generator(self):
         self.goal_pose=Pose()
-        self.goal_pose.position.x =-5.560
+        self.goal_pose.position.x =-4.560
         self.goal_pose.position.y =0.204
         self.goal_pose.position.z =0.0
         self.goal_pose.orientation.w=1.0
